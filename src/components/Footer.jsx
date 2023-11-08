867addf3eef6612f184b9ff599bc5b78985100fe
Nour Hijazi <nour.alhoda.hjz@gmail.com>
2023-11-08 12:24:59 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -89,7 +89,7 @@ import {
       <div className="w-full h-[1px] bg-white my-12" />
 
       <h6 className="text-white text-center text-base lg:text-lg">
-        &copy; Daunku - All rights reserved
+        &copy; Daunku - A Tutorial
       </h6>
       <h2 className="text-white text-center text-base lg:text-lg">Nour.H</h2>
     </div>
